b81ce7260d6ef9aea4b38829230ca5aa2470b351
Leonardo Lodi <your_email@address>
2024-04-30 18:16:32 +0100

    log the answers

--- src/components/Survey.jsx
+++ src/components/Survey.jsx
@@ -2,6 +2,20 @@ import { useState } from "react";
 
 function Survey() {
   const [open, setOpen] = useState(false); //Ignore this state
+  const [values, setValues] = useState([])
+  const [formData, setFormData] = useState({
+    rate: '',
+    spendTime: '',
+    feedback: '',
+    name: '',
+    email: ''
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setValues([...values, formData])
+    console.log(formData)
+  }
 
   return (
     <main className="survey">
@@ -10,11 +24,11 @@ function Survey() {
         {/* answers should go here */}
       </section>
       <section className="survey__form">
-        <form className="form">
+        <form onSubmit={(e) => handleSubmit(e)} className="form">
           <h2>Tell us what you think about your rubber duck!</h2>
           <div className="form__group radio">
             <h3>How do you rate your rubber duck colour?</h3>
-            <ul>
+            <ul onChange={(e) => setFormData({ ...formData, rate: e.target.value})}>
               <li>
                 <input id="color-one" type="radio" name="color" value="1" /><label
                   htmlFor="color-one"
@@ -43,7 +57,7 @@ function Survey() {
           </div>
           <div className="form__group">
             <h3>How do you like to spend time with your rubber duck</h3>
-            <ul>
+            <ul onChange={(e) => setFormData({ ...formData, spendTime: e.target.value})}>
               <li>
                 <label
                   ><input
@@ -80,17 +94,20 @@ function Survey() {
               name="review"
               cols="30"
               rows="10"
+              onChange={(e) => setFormData({ ...formData, feedback: e.target.value})}
             ></textarea></label
           ><label
             >Put your name here (if you feel like it):<input
               type="text"
               name="username"
-              value="" /></label
+              value={formData.name} 
+              onChange={(e) => setFormData({ ...formData, name: e.target.value})} /></label
           ><label
             >Leave us your email pretty please??<input
               type="email"
               name="email"
-              value="" /></label
+              value={formData.email} 
+              onChange={(e) => setFormData({ ...formData, email: e.target.value})} /></label
           ><input className="form__submit" type="submit" value="Submit Survey!" />
         </form>
       </section>
